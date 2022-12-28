a256c52660032cacfb483264c0ec16ea7d1a5e86
Victor Mora <victorlvmora@gmail.com>
2022-12-28 09:49:11 -0300

    Created NomeUsuario

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { HelloWorld } from './HelloWorld'
 import { Logos } from './Logos'
+import { NomeUsuario } from './NomeUsuario'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,10 @@ function App() {
       {/*Componente abaixo responsavel pelo Hello World! exibido na pagina. 
         Alterando a propriedade 'texto', irá alterar na pagina.*/}
       <HelloWorld texto={"Hello World!"} />
+
+      {/*Componente abaixo responsavel pelo nome que é exibido na pagina. 
+        Alterando a propriedade 'nomeUsuario', irá alterar na pagina.*/}
+      <NomeUsuario nomeUsuario={"Victor Mora"} />
     </div>
   )
 }
---
+++ src/NomeUsuario.jsx
@@ -0,0 +1,5 @@
+export function NomeUsuario({nomeUsuario}) {
+    return (
+        <h1>Usuario = {nomeUsuario}</h1>
+    )
+}
